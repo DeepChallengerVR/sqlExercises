8d4431dffb73a858ad3125f1d78b1f20fa4cde88
eborchers <borchers.erik@gmail.com>
2016-11-11 21:17:06 -0500

    Update pg14.sql

--- pg14.sql
+++ pg14.sql
@@ -1,20 +1,14 @@
+(13 sloc)  199 Bytes
 SELECT 
-	facility_fk,
     lastname,
     firstname,
-    dob AS 'DOB',
-    race,
+    dob,
     id
-   
-FROM 
-	healthcare.patient
-WHERE 
-	facility_fk = '1' OR '2' OR '3' OR '5' OR '6'
-AND
-    race = '5' OR '6'
-AND
-	type = 'gender'
-AND subtype IN ('f'
-    
-
+FROM
+    healthcare.patient
+WHERE
+	gender = 'F' AND
+    deceased_flag = 'N' AND
+    facility_fk IN('1','2','3','5','6') AND
+    race IN('5','6')
 ;
