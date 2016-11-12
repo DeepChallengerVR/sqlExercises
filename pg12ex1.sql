25b60e0330378e5038acb97a736d8250c2ef1e08
eborchers <borchers.erik@gmail.com>
2016-11-11 21:19:20 -0500

    Update pg12ex1.sql

--- pg12ex1.sql
+++ pg12ex1.sql
@@ -1,12 +1,13 @@
 SELECT 
-    transdate AS 'Transaction Date',
-    description AS 'Transaction Type',
-    symbol AS 'Security',
-    quantity AS 'Qty',
-    price AS 'PPS'
+    transdate AS "Transaction Date",
+    description AS "Transaction Type",
+    symbol AS "Security",
+    quantity AS "Qty",
+    price AS "PPS"
 FROM
     financial.security_transactions
-WHERE 
-	description LIKE 'Bought%'
-AND 
-	DATE(transdate) >= '2001-08-01' AND DATE(transdate) <= '2002-03-01'
+Where
+	description LIKE '%Bought%' AND
+    DATE(transdate) >= '2001-08-01' AND
+    DATE(transdate) < '2002-03-01'
+;
